b398104e34eb3f277e1ace3e23fdcdb3e1d66843
Joely Sousa Silva <joely@ubots.com.br>
2024-04-01 23:03:06 -0300

    Initialize Repository

---
+++ src/main/java/com/api/mensagefacebook/MensageFacebookApplication.java
@@ -0,0 +1,13 @@
+package com.api.mensagefacebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensageFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensageFacebookApplication.class, args);
+	}
+
+}
