4419d3be291827e54b527b181b461522168a0455
Joely <sjoely131@gmail.com>
2024-04-03 19:08:13 -0300

    Teste falhado da validação.

--- src/main/java/com/api/mensagefacebook/controller/WebhookController.java
+++ src/main/java/com/api/mensagefacebook/controller/WebhookController.java
@@ -1,2 +1,24 @@
-package com.api.mensagefacebook.controller;public class WebhookController {
+package com.api.mensagefacebook.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebhookController {
+
+    @GetMapping("/webhook")
+    public ResponseEntity<String> verifyWebhook(
+            @RequestParam("hub.mode") String mode,
+            @RequestParam("hub.verify_token") String verifyToken,
+            @RequestParam("hub.challenge") String challenge
+    ) {
+        if ("subscribe".equals(mode) && "EAAGEWHmp5oABO2MhOaIHa5kEIty9YF7grsdOfJLEdTWCP1BGGrZC9A74aTYUJ5ZBjvobqe61dZASSTWiuySehUv1Ncm2C7PULYUSkMOG6uzBJh0jLgYGI2mZAVZAFHwqpztkOVisLKc9E8JepuPuKHKRJVywSM0SFeBClZCkn5L4ioBC7lQSQanCkJMBjnnbX5oS3T8kAt5wZDZD".equals(verifyToken)) {
+            return ResponseEntity.status(HttpStatus.OK).body(challenge);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+    }
 }
